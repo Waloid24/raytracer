177b3023dcde4f6f13f3b1eb8c8c6be3d0be222b
Andrei Kazachkov <an.kazachkov3234@gmail.com>
2026-05-03 18:10:28 +0300

    Rendering has been updated through Color and Point3 objects

---
+++ src/color.rs
@@ -0,0 +1,19 @@
+use std::io::Write;
+
+use crate::vec3::Vec3;
+
+pub type Color = Vec3;
+
+pub fn write_color<W: Write>(out: &mut W, pixel_color: &Color) {
+    let r = pixel_color.x();
+    let g = pixel_color.y();
+    let b = pixel_color.z();
+
+    // Translate the [0,1] component values to the byte range [0,255].
+    let rbyte = (255.999 * r) as i32;
+    let gbyte = (255.999 * g) as i32;
+    let bbyte = (255.999 * b) as i32;
+
+    // Write out the pixel color components.
+    writeln!(out, "{} {} {}", rbyte, gbyte, bbyte).expect("failed to write color");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod vec3;
 mod color;
 
+use crate::color::{write_color, Color};
+
 fn main() {
 
     // Image
@@ -10,20 +12,15 @@ fn main() {
 
     //Render
 
+    let mut stdout = std::io::stdout();
+
     println!("P3\n{} {}\n255\n", image_width, image_height);
 
     for j in 0..image_height {
         eprintln!("\rScanlines remaining: {}", image_height-j);
         for i in 0..image_width {
-            let r = i as f64 / (image_width - 1) as f64;
-            let g = j as f64 / (image_height - 1) as f64;
-            let b = 0.0;
-
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
-
-            println!("{} {} {}", ir, ig, ib);
+            let pixel_color = Color::new((i as f64)/((image_width-1) as f64), (j as f64)/((image_height-1) as f64), 0.0);
+            write_color(&mut stdout, &pixel_color);
         }
     }
     eprintln!("\rDone.                 ");
